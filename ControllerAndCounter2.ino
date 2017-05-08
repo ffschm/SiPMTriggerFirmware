69b789324ac77f4593b4fe6f371c545206b8bdd5
Fabian P. Schmidt <fabian-schmidt@uni-bonn.de>
2017-05-08 20:36:47 +0200

    Initial commit.

---
+++ ControllerAndCounter2.ino
@@ -0,0 +1,74 @@
+volatile unsigned long irq_count;
+const int input_pin = 2;
+
+unsigned long integration_time = 1000;
+
+const size_t channels = 4;
+byte threshold[channels] = {128, 128, 128, 128};
+
+typedef enum {
+  constant,
+  scanning,
+  updated
+} mode_t;
+
+mode_t mode[channels] = {constant};
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("SiPMTrigger v4 Control v0.2");
+
+  setup_potentiometer();
+  setup_commands();
+
+  attachInterrupt(digitalPinToInterrupt(input_pin), IRQCounter, FALLING);
+}
+
+void loop() {
+  handle_commands();
+  set_thresholds();
+  const unsigned long counts = count_interrupts();
+  print_interrupts(counts);
+}
+
+void set_thresholds() {
+  for (size_t channel = 0; channel < 4; channel++) {
+    if (mode[channel] == scanning) {
+      threshold[channel]++;
+      // Write contents of serial register data to RDAC
+      sendCommand(1, channel, threshold[channel]);
+    }
+    else if (mode[channel] == updated) {
+      // Write contents of serial register data to RDAC
+      sendCommand(1, channel, threshold[channel]);
+      mode[channel] = constant;
+    }
+  }
+}
+
+unsigned long count_interrupts() {
+  cli();
+  irq_count = 0;
+  sei();
+  
+  delay(integration_time);
+  
+  cli();
+  const unsigned long _irq_count = irq_count;
+  sei();
+
+  return _irq_count;
+}
+
+void print_interrupts(const unsigned long counts) {
+
+  for (size_t channel = 0; channel < channels; channel++) {
+    Serial.print(threshold[channel]);
+    Serial.print(",");
+  }
+  Serial.println(counts);
+}
+
+void IRQCounter() {
+  irq_count++;
+}
---
+++ commands.ino
@@ -0,0 +1,64 @@
+String inputString = "";
+boolean stringComplete = false;
+
+void setup_commands() {
+  // Reserve bytes for the input string
+  inputString.reserve(20);
+}
+
+// Source: https://arduino.stackexchange.com/a/1237
+String getValue(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+void handle_commands() {
+  if (stringComplete) {
+    if (inputString.startsWith("SET")) {
+      // set potentiometer values
+      const byte channel = (byte) (getValue(inputString.substring(4), ',', 0).toInt());
+      const byte value = (byte) (getValue(inputString.substring(4), ',', 1).toInt());
+      if (0 <= channel && channel <= 4) {
+        if (0 <= value  && value <= 255) {
+          mode[channel] = updated;
+          threshold[channel] = value;
+        } else {
+          Serial.print("# SET command failed: Invalid value.");
+        }
+      } else {
+        Serial.print("# SET command failed: Invalid channel.");
+      }
+    } else if (inputString.startsWith("SCAN")) {
+      const byte channel = (byte) (getValue(inputString.substring(4), ',', 0).toInt());
+      if (0 <= channel && channel <= 4) {
+        mode[channel] = scanning;
+      } else {
+        Serial.print("# SCAN command failed: Invalid channel.");
+      }
+    }
+    inputString = "";
+    stringComplete = false;
+  }
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char) Serial.read();
+    inputString += inChar;
+
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}
