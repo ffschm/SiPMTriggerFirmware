34e26103e1a1937e2e4b61f5a8e39467e5669bcf
Fabian P. Schmidt <kerel-fs@gmx.de>
2018-02-22 23:57:53 +0100

    contrib/get_calibration.py: Generate example commands

--- contrib/get_calibration.py
+++ contrib/get_calibration.py
@@ -4,7 +4,8 @@ from argparse import ArgumentParser
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser(description='Calculate gain and offset from the first two photopeaks.')
+    parser = ArgumentParser(description='Calculate offset and gain from the first two photopeaks.')
+    parser.add_argument('-c', '--channel', type=int)
     parser.add_argument('thr_1pe', type=float)
     parser.add_argument('thr_2pe', type=float)
     args = parser.parse_args()
@@ -12,5 +13,10 @@ if __name__ == '__main__':
     gain = args.thr_2pe - args.thr_1pe
     offset = args.thr_1pe - gain
 
-    print(gain)
-    print(offset)
+    print("Offset: {}".format(offset))
+    print("Gain:   {}".format(gain))
+
+    if (args.channel and args.channel in [1,2]):
+        print("\nSend these commands to the SiPMTrigger Controller to set the gain and offset of this channel:")
+        print("SET OFFSET {}, {}".format(args.channel, offset))
+        print("SET GAIN {}, {}".format(args.channel, gain))
